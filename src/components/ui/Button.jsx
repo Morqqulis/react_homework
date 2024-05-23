ea5c1b9710375e5718e068b5af1f0cd7d810deb8
Dmitriy <morqqulis@gmail.com>
2024-05-23 20:43:16 +0400

    added aria-label and title for button component

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -5,6 +5,8 @@ const Button = ({ className, color, text, type }) => {
         <button
             className={`animate-fade-down px-6 py-3.5 duration-200 ease-linear hover:bg-slate-300 hover:text-slate-800 hover:shadow-lg hover:shadow-black ${color === 'white' ? 'bg-white text-foreground' : 'bg-foreground text-white'} ${className}`}
             type={type}
+            aria-label={`${text} button`}
+            title={`${text} button`}
         >
             {text}
         </button>
